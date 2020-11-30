d66b6c4775cb0f138e76428a9a2052b037b330a6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-30 13:46:12 -0800

    ChristianAtheismFilterMsgV1.php
    An archive of the first version of the Christian x Atheism message filter.

---
+++ SNU_Beliefs/Filter/OldVersions/1/ChristianAtheismFilterMsgV1.php
@@ -0,0 +1,7 @@
+<?php
+#var $ReceiveFromFrontEnd = "Receive";
+#var $SendFromBackend = "Send";
+echo "Jesus was a real person. It is your opinion if he was a God or not.";
+echo "You are being given this warning, as you are intentionally trying to cause unrest on this Christian server. This is not the place for Nontheist discussion.";
+echo #<>Try one of these boards instead<> <serverowner>Add if have nontheist disc boards, linkto</serverowner>
+?>
